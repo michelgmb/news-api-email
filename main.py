08b0b0f72f0a4e8efd625187c21d234637d35f94
michelgmb <michelgm276@gmail.com>
2023-10-11 21:53:59 -0400

    added smtp and email

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import requests
 
-api_key ="dee847eb9f494434acfda9bab5fa5d60"
+from  smtp import message_send
+
+
+api_key = "dee847eb9f494434acfda9bab5fa5d60"
 # url = "https://finance.yahoo.com"
 
-#endpoint :
-url = "https://newsapi.org/v2/everything?q=tesla&from"\
-    "=2023-09-11&sortBy=publishedAt&apiKey="\
-    "dee847eb9f494434acfda9bab5fa5d60"
+# endpoint :
+url = "https://newsapi.org/v2/everything?q=tesla&from" \
+      "=2023-09-11&sortBy=publishedAt&apiKey=" \
+      "dee847eb9f494434acfda9bab5fa5d60"
 # make request
 request = requests.get(url)
 # content = request.text
 #
 # print(type(content))
 
-#to turned it to a manipulative
+# to turned it to a manipulative
 # Get dictionary with data
 content = request.json()
 # print(type(content))
 # print(content['articles'])
 articles = content['articles']
+
+body = " "
 for article in articles:
-    print(article['title'])
-    print(article['description'])
-    print("<------------------------------>")
+    if article["title"] is not None:
+        body = body + article['title'] + '\n' + article['description'] + 2*'\n'
+
+body = body.encode("utf-8")
+message_send(message=body)
---
+++ smtp.py
@@ -0,0 +1,40 @@
+import smtplib, ssl, os
+
+
+"""
+import smtplib, ssl
+
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "my@gmail.com"  # Enter your address
+receiver_email = "your@gmail.com"  # Enter receiver address
+password = input("Type your password and press enter: ")"""
+"""
+message = ""\
+Subject: Hi there
+
+This message is sent from Python.""
+
+context = ssl.create_default_context()
+with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+    server.login(sender_email, password)
+    server.sendmail(sender_email, receiver_email, message)
+    """
+
+def message_send(message):
+    port = 465  # For SSL
+
+    smtp_server = "smtp.gmail.com"
+    sender_email = 'devopsesi2022@gmail.com'
+    receiver_email = 'devopsesi2022@gmail.com'
+    password = 'unkj vlla dtat dxkd'  # os.getenv("PASSWORD")
+
+    # Create a secure SSL context
+    context = ssl.create_default_context()
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+
+#send_message()
